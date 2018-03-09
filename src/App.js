ecf5fc69fd81af32da22621e4294dec84071c53b
Jerome Chery <jerome.chery@nrco.fr>
2018-03-09 15:05:36 +0100

    QueryString

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import Grid from "./components/Grid";
 
 class App extends React.Component {
+  getUrlParameter(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(window.location.search);
+    return results === null
+      ? null
+      : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
   render() {
-    const qs = {
-      x: 9,
-      y: 9,
-      show: true
-    };
-    return <Grid x={qs.x} y={qs.y} show={qs.show} />;
+    let x = this.getUrlParameter('x');
+    let y = this.getUrlParameter('y');
+    let show = this.getUrlParameter('show');
+    return <Grid x={x ? x : 9} y={y ? y : 9} show={show ? show  : false} />;
   }
 }
 
